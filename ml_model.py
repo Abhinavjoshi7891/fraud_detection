d3c5fdc44e51b531b438c6e80ca066b009bf9055
Abhinavjoshi7891 <abhinavjoshi7891@gmail.com>
2022-01-13 10:52:16 +0530

    model2 is the final model

--- ml_model.py
+++ ml_model.py
@@ -10,3 +10,14 @@ def model2():
 
 def model3():
     print("adding code for ML model3")
+
+##def model1():print("adding code for ML model1")
+##def model2():print("adding code for ML model2")
+##def model3():print("adding code for ML model3")
+
+
+## lets say that model2 is giving the best perfromance
+## So we will use that as the final model
+
+print('model2 is performing the best')
+## we will commit and push this model into the master
