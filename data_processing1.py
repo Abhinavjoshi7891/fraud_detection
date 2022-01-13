cd441e90a280638a6c1504c0c6218aa77795cd1a
Abhinavjoshi7891 <abhinavjoshi7891@gmail.com>
2022-01-13 06:39:52 +0530

    adding data processing python code

---
+++ data_processing1.py
@@ -0,0 +1,6 @@
+
+print("adding all the processing files related to fraud detection")
+
+##  we need to push this file to the github repository so that  when we come to thte fraud_detection repository, this new data_processing python file is also there
+## for that we will follow the same steps that we did for the README.md file in the terminal
+## let's move to the terminal
